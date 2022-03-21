382268b6a3eb2e37a92be58cc445757485ed2f06
Milica <pavlovicm995@yahoo.com>
2022-03-21 21:55:43 +0100

    Added component AppCars and CarService

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import AppCars from "./AppCars";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to="/cars">Cars</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/cars">
+            <AppCars/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
