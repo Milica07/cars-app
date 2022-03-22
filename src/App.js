a71eed60364ceb7b895bbd2330c29996bb3572a5
Milica <pavlovicm995@yahoo.com>
2022-03-22 10:12:17 +0100

    Edited component AppCars and pages

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
-import AppCars from "./AppCars";
+import AppCars from "./pages/AppCars";
 
 function App() {
   return (
